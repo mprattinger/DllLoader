5c715c5ca41544a15189d4185efadcc93e2081dd
Michael Prattinger <mprattinger@outlook.com>
2017-11-14 23:04:56 +0100

    wip

--- DllLoader/Loader/PluginLoader.cs
+++ DllLoader/Loader/PluginLoader.cs
@@ -9,38 +9,71 @@ namespace DllLoader.Loader
 {
     public class PluginLoader
     {
-        public List<string> PluginDlls { get; set; }
-        public List<Assembly> Assemblies { get; set; }
         public List<PluginStructure> Plugins { get; set; }
 
         public PluginLoader()
         {
-            Assemblies = new List<Assembly>();
-            Plugins = new List<PluginStructure>();
         }
 
         public void Init()
         {
-            var pluginsPath = Path.Combine(Directory.GetCurrentDirectory(), "plugins");
-            PluginDlls = Directory.EnumerateFiles(pluginsPath).ToList();
-
-            PluginDlls.ForEach(p => {
-                using(var ar = new AssemblyResolver(p))
-                {
-                    Assemblies.Add(ar.Assembly);
-                    var plugin = Activator.CreateInstance(ar.Assembly.GetTypes().First()) as IPlugin; //Class-Type anhand des Namens ermitteln!!!!
-                    var inf = plugin.GetPluginInfo();
-                    Plugins.Add(new PluginStructure { UniqueName = inf.UniquePluginName, Info = inf, Plugin = plugin });
-                }
-            });
+            var dlls = getDllFiles();
+            var assem = resolveAssemblies(dlls);
+            Plugins = initPlugins(assem);
         }
 
         public void Execute()
         {
-            Assemblies.ForEach(a => {
-                var plugin = Activator.CreateInstance(a.GetTypes().First()) as IPlugin;
-                plugin.Run();
+            Plugins.ForEach(p =>
+            {
+                p.Plugin.Run();
             });
+        }
+
+        private List<string> getDllFiles()
+        {
+            var pluginsPath = Path.Combine(Directory.GetCurrentDirectory(), "plugins");
+            return Directory.EnumerateFiles(pluginsPath).ToList();
+        }
+
+        private List<Assembly> resolveAssemblies(List<string> dlls)
+        {
+            var ret = new List<Assembly>();
+            dlls.ForEach(p =>
+            {
+                using (var ar = new AssemblyResolver(p))
+                {
+                    ret.Add(ar.Assembly);
+                }
+            });
+            return ret;
+        }
+
+        private List<PluginStructure> initPlugins(List<Assembly> assemblies)
+        {
+            var ret = new List<PluginStructure>();
+            assemblies.ForEach(a => {
+                try
+                {
+                    getType(a);
+                    var type = getType(a);
+                    var plugin = Activator.CreateInstance(type) as IPlugin; //Class-Type anhand des Namens ermitteln!!!!
+                    var inf = plugin.GetPluginInfo();
+                    ret.Add(new PluginStructure { UniqueName = inf.UniquePluginName, Info = inf, Plugin = plugin });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error initializing plugin {a.FullName}, {ex.Message}");
+                }
+            });
+            return ret;
+        }
+
+        private Type getType(Assembly assembly)
+        {
+            var name = assembly.ManifestModule.Name;
+            var className = name.Substring(0, name.IndexOf("Plugin"));
+            return assembly.GetTypes().Where(t => t.Name.Contains(className)).FirstOrDefault();
         }
     }
 }
--- DllLoader/Program.cs
+++ DllLoader/Program.cs
@@ -9,7 +9,14 @@ namespace DllLoader
         {
             var pLoader = new PluginLoader();
             pLoader.Init();
-            pLoader.Execute();
+            //pLoader.Execute();
+
+            Console.WriteLine("DllLoader v1.0.0-alpha (c) MPrattinger 2017");
+            Console.WriteLine("===========================================");
+            Console.WriteLine("");
+            pLoader.Plugins.ForEach(p => {
+                Console.WriteLine($"{p.Info.LongName} / {p.Info.ShortName} => {p.Info.UniquePluginName}");
+            });
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
