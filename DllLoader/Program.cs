4f26ee5050578845fbfea465471230c62db2d419
Michael Prattinger <mprattinger@outlook.com>
2017-11-15 16:06:08 +0100

    wip

--- DllLoader/Program.cs
+++ DllLoader/Program.cs
@@ -1,5 +1,7 @@
 ﻿using DllLoader.Loader;
+using DllLoader.Utils;
 using System;
+using System.Reflection;
 
 namespace DllLoader
 {
@@ -9,17 +11,64 @@ namespace DllLoader
         {
             var pLoader = new PluginLoader();
             pLoader.Init();
-            //pLoader.Execute();
 
-            Console.WriteLine("DllLoader v1.0.0-alpha (c) MPrattinger 2017");
-            Console.WriteLine("===========================================");
-            Console.WriteLine("");
-            pLoader.Plugins.ForEach(p => {
-                Console.WriteLine($"{p.Info.LongName} / {p.Info.ShortName} => {p.Info.UniquePluginName}");
-            });
+
+
+            ////pLoader.Execute();
+
+            //Console.WriteLine("DllLoader v1.0.0-alpha (c) MPrattinger 2017");
+            //Console.WriteLine("===========================================");
+            //Console.WriteLine("");
+            //pLoader.Plugins.ForEach(p => {
+            //    Console.WriteLine($"{p.Info.LongName} / {p.Info.ShortName} => {p.Info.UniquePluginName}");
+            //});
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
+        }
+
+        public static int ProcessArgs(string[] args)
+        {
+            var lastArg = 0;
+            for (; lastArg < args.Length; lastArg++)
+            {
+                if (IsArg(args[lastArg], "h", "help"))
+                {
+
+                    return 0;
+                }
+                else if (IsArg(args[lastArg], "version"))
+                {
+                    printVersion();
+                    return 0;
+                }
+                else if (IsArg(args[lastArg], "info"))
+                {
+                    printInfo();
+                    return 0;
+                }
+            }
+            return 1;
+        }
+
+        private static void printVersion()
+        {
+            Console.WriteLine(Product.Version);
+        }
+
+        private static void printInfo()
+        {
+            Console.WriteLine(Product.Version);
+        }
+
+        private static bool IsArg(string candidate, string longName)
+        {
+            return IsArg(candidate, shortName: null, longName: longName);
+        }
+
+        private static bool IsArg(string candidate, string shortName, string longName)
+        {
+            return (shortName != null && candidate.Equals("-" + shortName)) || (longName != null && candidate.Equals("--" + longName));
         }
     }
 }
---
+++ DllLoader/Utils/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace DllLoader.Utils
+{
+    public class Product
+    {
+        //public static string LongName => LocalizableStrings.DotNetCommandLineTools;
+        public static readonly string Version = GetProductVersion();
+
+        private static string GetProductVersion()
+        {
+            var attr = typeof(Product)
+                .GetTypeInfo()
+                .Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            return attr?.InformationalVersion;
+        }
+    }
+}
